b1edb053467b6bb96be4380c307e8aec4f706339
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2023-10-19 21:20:45 +1300

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,19 +1,49 @@
-apt update
+# apt update
 apt install unzip -y
 # apt install language-pack-zh-hans-base  language-pack-zh-hans -y
+
+wget https://github.com/hello-world-1989/proxy/archive/refs/heads/main.zip
+mv main.zip proxy.zip
+
+unzip proxy.zip
+cd proxy-main
+mkdir public
+
+sudo sh nodesource_setup.sh
 
 # sudo dpkg-reconfigure --force locales
 
 # Using Ubuntu
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-wget https://github.com/hello-world-1989/proxy/archive/refs/heads/main.zip
-
-unzip main.zip
-cd proxy-main
+# sudo snap install node --classic
+sudo apt install nodejs -y
+node -v
+#need reboot
 
 npm install
+# npm install -g npm@10.2.1
+npm install -g  pm2 typescript
 
 echo "export PORT=80" >> ~/.bashrc
 source ~/.bashrc
+
+# ---------------run every update--------------------------
+tsc ~/proxy-main/src/proxy.ts
+pm2 start ~/proxy-main/src/proxy.js -n app
+# -----------------------------------------
+
+
+pm2 save
+pm2 startup
+
+
+echo '30 20 * * * root sudo reboot' >> /etc/crontab
+sudo service cron start
+
+cd ~
+
+wget https://github.com/hello-world-1989/temp/archive/refs/heads/main.zip
+
+mv main.zip temp.zip
+unzip temp.zip
+
+mv temp-main ./proxy-main/public
